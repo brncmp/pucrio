6ea7ff35dc89734d93e377bad77cdb98fffc8503
Bruno Campos <brunocampos_br@hotmail.com>
2023-07-13 20:47:32 -0300

    mensagem de texto inicial (modal)

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,7 +4,14 @@
       <v-col md6>
         <UsersList />
       </v-col>
-      <v-col md6>
+      <v-col class="w-100" md6>
+      <Modal 
+        title="Bem-vindo!"
+        :items="discount_type"
+        button="Cupom do mês"
+        modalType="div"
+        class="float-right"
+      />
         
       </v-col>
     </v-row>
@@ -13,10 +20,28 @@
 
 <script>
 import UsersList from './UsersList.vue'
+import Modal from '../components/Modal.vue'
 
 export default {
   components: {
-    UsersList
+    UsersList, Modal
   },
+  computed: {
+    discount_type () {
+      const month = new Date().getMonth()
+        if (month % 2 === 0) {
+        return this.mensagem.text
+      } else {
+         return this.mensagem.text2
+      }
+    }
+  },
+  data: () => ({
+    mensagem: {
+            "text": "Olá, hoje o seu acesso te oferece um cupom de 15% de desconto em nossos produtos. Acesse nosso instagram e veja o cupom disponível hoje! @brncmp",
+            "text2": "Olá, hoje o seu acesso te oferece um cupom de 20% de desconto em nossos produtos. Acesse nosso instagram e veja o cupom disponível hoje! @brncmp"
+        },
+    
+  }),
 };
 </script>
